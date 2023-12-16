e1c13ec6030f2f4963c7f6cc4430b7b8eb55e434
Pablo Rodrigues <pablorodrigues606@gmail.com>
2023-12-16 15:09:46 -0800

    Commit final

--- app.py
+++ app.py
@@ -6,22 +6,21 @@ driver = webdriver.Chrome()
 driver.get('https://www.novaliderinformatica.com.br/computadores-gamers')
 
 # extrair todos os textos
-titulos = driver.find_elements(By.XPATH,'//a[@class="nome-produto"]')
+titulos = driver.find_elements(By.XPATH,"//a[@class='nome-produto']")
 
 # extraindo os preços
-precos = driver.find_elements(By.XPATH, '//strong[@class="preco-promocional"]')
+precos = driver.find_elements(By.XPATH, "//strong[@class='preco-promocional']")
 
 # criando a planilha
 workbook = openpyxl.Workbook()
-workbook.create_sheet('Produtos')
+workbook.create_sheet('produtos')
 # criando a página
-sheet_produtos = workbook['Produtos']
+sheet_produtos = workbook['produtos']
 sheet_produtos['A1'].value = 'Produto'
 sheet_produtos['B1'].value = 'Preço'
 
-
 # inserindo os títulos preços na planilha
 for titulo, preco in zip(titulos, precos):
-    sheet_produtos.append([])
+    sheet_produtos.append([titulo.text, preco.text])
 # exportando a planilha
 workbook.save('produtos.xlsx')
